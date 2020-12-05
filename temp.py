af7efcc2c3cd33449e7edb9d0b447a6439dbcb7d
hemanta212 <sharmahemanta.212@gmail.com>
2020-12-05 17:45:55 +0000

    WIP: Add basic info displayer : assignment 2 (heat)

--- temp.py
+++ temp.py
@@ -9,32 +9,39 @@ class Test(Scene):
 
     def thermometric_liquids(self):
         intro = (
-            "Thermometric Liquid",
-            "Liquid that are use inside thermometer are called",
-            "thermometeric liquid",
+            "Thermometric Liquids",
+            "Liquids that are use inside thermometer",
+            "are called thermometeric liquids",
         )
-        self.display_simple_info(*intro)
+        color_map = {0: {"Liquids": ORANGE}, 2: {"are": BLUE}}
+        self.display_simple_info(*intro, color_map=color_map)
 
     def display_simple_info(
         self,
         *args,
         scale=1.0,
+        color_map={},
         exit_animation=FadeOutAndShiftDown,
         spacing=0.5,
         run_time=3,
-        wait_at_last=1.0
+        wait_at_last=1.0,
     ):
-        texts = [TextMobject(i) for i in args]
-        title = texts[0]
-        title.to_edge(UP)
-        title.set_color(BLUE)
-        self.play(Write(title), run_time=3)
-        self.wait()
+        singlify = lambda x: [f"{i} " for i in x.split(" ")]
+        texts = [TextMobject(*singlify(i)) for i in args]
+        get_color_map = lambda i: color_map.get(i) if color_map(i) else {}
 
-        processed = [title]
-        for text in texts[1:]:
-            text.next_to(processed[-1], DOWN, buff=spacing)
+        def get_styled_title(title):
+            title.scale(1.5)
+            title.to_edge(UP)
+            title.set_color(BLUE)
+
+        processed = []
+        for index, text in enumerate(texts):
+            if not index == 0:
+                text.next_to(processed[-1], DOWN, buff=spacing)
             text.scale(scale)
+            text = get_styled_title(text) if index == 0 else text
+            text.set_color_by_text_to_color_map(get_color_map(index))
             self.play(Write(text), run_time=run_time)
             processed.append(text)
 
