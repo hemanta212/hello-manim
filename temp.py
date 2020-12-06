f8f2c154d53ec1bd1923ef7bf0b61077adb8ef8d
hemanta212 <sharmahemanta.212@gmail.com>
2020-12-06 13:59:00 +0000

    WIP: adding image display: assignment 2 (heat)

--- temp.py
+++ temp.py
@@ -6,6 +6,15 @@ class Test(Scene):
         #        img = SVGMobject("thermometer.svg")
         #        self.play(DrawBorderThenFill(img, rate_func=linear))
         self.thermometric_liquids()
+        # self.test()
+
+    def test(self):
+        text = TextMobject("Test")
+        self.add(text)
+
+        # self.play(Indicate(text, color=GREEN_A, run_time=3))
+        self.play(TurnInsideOut(text))
+        self.wait()
 
     def thermometric_liquids(self):
         intro = (
@@ -23,11 +32,22 @@ class Test(Scene):
         }
         self.display_simple_info(*intro, color_map=color_map)
 
+    def clinical_thermometer(self):
+        intro = [
+            "Clinical thermometer: (SVG image of the thermometer)",
+            "Range of temperature.",
+            "Degree Celsius: 35 to 42",
+            "Degree Fahrenheit: 94 to 108",
+            "It is used to measure the temperature of the human body.",
+        ]
+        color_map = {}
+
     def display_simple_info(
         self,
         *args,
         scale=1.0,
         color_map={},
+        image=None,
         exit_animation=FadeOutAndShiftDown,
         spacing=0.5,
         run_time=3,
@@ -41,15 +61,18 @@ class Test(Scene):
             title.scale(1.5)
             title.to_edge(UP)
             title.set_color(BLUE)
+            title.set_color_by_tex_to_color_map(get_color_map(0))
             return title
 
-        processed = []
-        for index, text in enumerate(texts):
-            if not index == 0:
-                text.next_to(processed[-1], DOWN, buff=spacing)
-                text.to_edge(LEFT)
+        title = get_styled_title(texts[0])
+        self.play(Write(title), run_time=run_time)
+
+        processed = [title]
+        paragraphs = Group(*texts[1:]).arrange(DOWN)
+        for index, text in enumerate(texts[1:]):
+            index += 1
+            text.next_to(processed[-1], DOWN, buff=spacing)
             text.scale(scale)
-            text = get_styled_title(text) if index == 0 else text
             text.set_color_by_tex_to_color_map(get_color_map(index))
             self.play(Write(text), run_time=run_time)
             processed.append(text)
