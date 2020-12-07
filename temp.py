98d667f0b0b07cbaef276a6a6554b0db30bd8bfc
hemanta212 <sharmahemanta.212@gmail.com>
2020-12-07 16:58:08 +0000

    WIP: Improve display layout info : assignment 2 (heat)

--- temp.py
+++ temp.py
@@ -3,13 +3,15 @@ from manimlib.imports import *
 
 class Test(Scene):
     def construct(self):
-        # self.clinical_thermometer()
-        self.intro()
+        # self.thermometric_liquids()
+        self.clinical_thermometer()
+        # self.intro()
 
     def intro(self):
         topic = TextMobject("Thermometer")
         image = ImageMobject("thermometer.jpg")
         desc = TextMobject("A device used to measure temperature")
+        topic.scale(1.5)
         desc.next_to(image, DOWN)
         self.play(Write(topic))
         self.play(ApplyMethod(topic.next_to, image, 2 * UP))
@@ -18,31 +20,62 @@ class Test(Scene):
         self.wait(2)
 
     def thermometric_liquids(self):
+        GAP = "{{GAP}}"
         intro = (
             "Thermometric Liquids",
+            GAP,
             "Liquids that are used inside thermometer are called",
             "thermometeric liquids.",
+            GAP,
             "Examples of thermometric liquids,",
             "1. Mercury",
             "2. Alcohol",
         )
         color_map = {
-            1: {"inside": ORANGE, "thermometer": ORANGE},
-            5: {"Alcohol": ORANGE},
-            4: {"Mercury": BLUE},
+            2: {"inside": ORANGE, "thermometer": ORANGE},
+            5: {"Mercury": BLUE},
+            6: {"Alcohol": ORANGE},
         }
-        self.display_simple_info(*intro, color_map=color_map)
+        self.display_simple_info(*intro, color_map=color_map, center=True)
 
-    def clinical_thermometer(self):
+    def laboratory_thermometer(self):
         intro = [
             "Clinical thermometer",
             "Range of temperature.",
-            "Degree Celsius: 35 $^{\circ}$ to 42",
-            "Degree Fahrenheit: 94 to 108",
-            "It is used to measure the temperature of the human body.",
+            "- Celsius: 35$^{\circ}$C to 42$^{\circ}$C",
+            "- Fahrenheit: 94$^{\circ}$F to 108$^{\circ}$F",
+            "It is used to measure the temperature",
+            "of the human body.",
         ]
-        color_map = {}
-        self.display_simple_info(*intro, image="test.svg")
+        color_map = {
+            2: {"35": ORANGE, "42": ORANGE},
+            3: {"94": ORANGE, "108": ORANGE},
+            5: {"human": BLUE, "body": BLUE},
+        }
+        self.display_simple_info(
+            *intro, image="clinical.jpg", image_scale=2.0, color_map=color_map
+        )
+
+    def clinical_thermometer(self):
+        GAP = "{{GAP}}"
+        intro = [
+            "Clinical thermometer",
+            GAP,
+            "Range of temperature:",
+            "- Celsius: 35$^{\circ}$C to 42$^{\circ}$C",
+            "- Fahrenheit: 94$^{\circ}$F to 108$^{\circ}$F",
+            GAP,
+            "It is used to measure the temperature",
+            "of the human body.",
+        ]
+        color_map = {
+            3: {"35": ORANGE, "42": ORANGE},
+            4: {"94": ORANGE, "108": ORANGE},
+            7: {"human": BLUE, "body": BLUE},
+        }
+        self.display_simple_info(
+            *intro, image="clinical.jpg", image_scale=2.0, color_map=color_map
+        )
 
     def display_simple_info(
         self,
@@ -50,9 +83,11 @@ class Test(Scene):
         scale=1.0,
         color_map={},
         image=None,
+        image_scale=1.0,
         exit_animation=FadeOutAndShiftDown,
         spacing=0.5,
         run_time=3,
+        center=False,
         wait_at_last=1.0,
     ):
         singlify = lambda x: [f"{i} " for i in x.split(" ")]
@@ -72,22 +107,25 @@ class Test(Scene):
         processed = [title]
 
         if image:
-            svg_image = SVGMobject(image, stroke_color=WHITE).to_edge(RIGHT)
-            self.add(svg_image)
+            image_ = ImageMobject(image).to_edge(RIGHT)
+            image_.scale(image_scale)
+            self.add(image_)
 
         body_texts = texts[1:]
-        paragraphs = VGroup(*body_texts).arrange(DOWN)
         for index, text in enumerate(texts[1:]):
             index += 1
+            is_gap = text.tex_string.strip() == "{{GAP}}"
             text.next_to(processed[-1], DOWN, buff=spacing)
+            text = text.to_edge(LEFT) if not center else text
             text.scale(scale)
             text.set_color_by_tex_to_color_map(get_color_map(index))
-            self.play(Write(text), run_time=run_time)
+            text = text.set_color(BLACK) if is_gap else text
+            self.play(Write(text), run_time=run_time) if not is_gap else print("")
             processed.append(text)
 
         self.wait(wait_at_last)
-        all_texts = Group(*processed)
-        self.play(exit_animation(all_texts))
+        contents = Group(*self.mobjects, *self.foreground_mobjects)
+        self.play(exit_animation(contents))
 
 
 SCENES_IN_ORDER = [Test]
