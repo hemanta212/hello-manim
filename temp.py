9d02df3d8b5f84fe03612720e3b8b4cf43ed685a
hemanta212 <sharmahemanta.212@gmail.com>
2020-12-18 08:26:00 +0000

    WIP: finished first section: Assignemnt 4 (hydrocarbons)

--- temp.py
+++ temp.py
@@ -1,55 +1,66 @@
 from manimlib.imports import *
 
 
-class SaturatedHydrocarbon(Scene):
+class Test(Scene):
     # placeholder for leaving gap between Mobjects
     GAP = "{{GAP}}"
-    saturated_topic = None
+    alkene_topic = None
     general_formula = None
-    molecular_formula = None
 
     def construct(self):
-        self.staturated_intro()
-        self.word_roots_table()
-        self.hydrocarbons_properties()
+        self.unsaturated_hydrocarbon_intro()
+        self.alkene_intro()
+        self.alkene_mol_formulas()
+        self.alkene_examples()
 
-    def staturated_intro(self):
-        self.intro()
-        self.saturated_hydrocarbon_intro()
-        self.saturated_hydrocarbon_mol_formulas()
-
-    def intro(self):
+    def unsaturated_hydrocarbon_intro(self):
         intro = (
-            "An Introduction to Hydrocarbons",
-            self.GAP,
-            "Types of hydrocarbons:",
-            "$\circ$ Saturated hydrocarbons",
-            "$\circ$ Unsaturated hydrocarbons",
+            "Unsaturated Hydrocarbons",
+            "Those hydrocarbons where all carbon to carbon bonds are",
+            "double or triple covalent bonds are unsaturated hydrocarbons",
+            "Types of unsaturated hydrocarbons:",
+            "1. Alkenes",
+            "2. Alkynes",
         )
+
         color_map = {
-            3: {"Saturated": ORANGE},
-            4: {"Unsaturated": ORANGE},
+            2: {
+                "double": ORANGE,
+                "or": ORANGE,
+                "triple": ORANGE,
+                "covalent": ORANGE,
+                "bond": ORANGE,
+            },
+            4: {"Alkenes": BLUE},
+            5: {"Alkynes": BLUE},
         }
 
-        self.saturated_topic = self.display_simple_info(
-            *intro, color_map=color_map, skip_exit_anim_and_return=[3]
+        self.alkene_topic = self.display_simple_info(
+            *intro,
+            color_map=color_map,
+            skip_exit_anim_and_return=[4],
         )[0]
 
-        topic = TextMobject("Saturated Hydrocarbons")
-        topic.move_to(ORIGIN).to_edge(UP).scale(1.5).set_color(BLUE)
-        self.play(Transform(self.saturated_topic, topic))
+        new_alkene_title = TextMobject("Alkenes", color=BLUE).scale(1.5)
+        new_alkene_title.to_edge(UP)
+        self.play(Transform(self.alkene_topic, new_alkene_title))
+        self.wait()
 
-    def saturated_hydrocarbon_intro(self):
+    def alkene_intro(self):
+#        self.alkene_topic = TextMobject("Alkenes", color=BLUE).scale(1.5)
+#        self.alkene_topic.to_edge(UP)
         intro = (
-            self.saturated_topic,
-            "Those hydrocarbons where all carbon to carbon bonds",
-            "are single covalent bond are saturated hydrocarbons",
-            "Its general formula is $C_{n}H_{2n+2}$",
+            self.alkene_topic,
+            "Those hydrocarbons in which at least one carbon to carbon",
+            "bond is double covalent bond while the rest maybe double",
+            "or single covalent bonds are called alkenes.",
+            "Its general formula is $C_{n}H_{2n}$ where n>1",
         )
 
         color_map = {
-            2: {"single": ORANGE, "covalent": ORANGE, "bond": ORANGE},
-            3: {"C": BLUE},
+            1: {"at": ORANGE, "least": ORANGE, "one": ORANGE},
+            2: {"double": ORANGE, "covalent": ORANGE},
+            4: {"C": BLUE, "n>1": ORANGE},
         }
 
         self.general_formula = self.display_simple_info(
@@ -60,54 +71,191 @@ class SaturatedHydrocarbon(Scene):
             skip_exit_anim_and_return=[i for i in range(len(intro))],
         )[-1]
 
-    def saturated_hydrocarbon_mol_formulas(self):
+    def alkene_mol_formulas(self):
         self.molecular_formula = TextMobject("Molecular formulas;")
-        self.molecular_formula.next_to(self.general_formula, DOWN, buff=1.0).to_edge(
+        self.molecular_formula.next_to(self.general_formula, DOWN, buff=0.6).to_edge(
             LEFT
         )
         self.play(Write(self.molecular_formula))
         gen_formula = (
             "C_n",
-            "H_{2n+1}",
+            "H_{2n}",
         )
-        methane = [
-            "methane",
-            "take n = 1",
-            gen_formula,
-            ("C_1", "H_{2 \\times 1 + 2}"),
-            ("C", "H_{4},"),
-        ]
-
-        ethane = [
-            "ethane",
+        ethene = [
+            "ethene",
             "take n = 2",
             gen_formula,
-            ("C_2", "H_{2 \\times 2 + 2}"),
-            ("C_2", "H_{6},"),
+            ("C_2", "H_{2 \\times 2}"),
+            ("C_2", "H_{4},"),
         ]
-        propane = [
-            "propane",
+        propene = [
+            "propene",
             "take n = 3",
             gen_formula,
-            ("C_3", "H_{2 \\times 3 + 2}"),
-            ("C_3", "H_{8},"),
+            ("C_3", "H_{2 \\times 3}"),
+            ("C_3", "H_{6},"),
         ]
-        butane = [
-            "butane",
+        butene = [
+            "butene",
             "take n = 4",
             gen_formula,
-            ("C_4", "H_{2 \\times 4 + 2}"),
-            ("C_4", "H_{10},"),
+            ("C_4", "H_{2 \\times 4}"),
+            ("C_4", "H_{8},"),
         ]
 
         processed = []
-        for molecule in (methane, ethane, propane, butane):
+        for molecule in (ethene, propene, butene):
             last_molecule = processed[-1] if processed else None
             formula = self.show_molecular_animations(molecule, last_molecule)
             processed.append(formula)
 
+        self.wait()
         all_contents = Group(*self.mobjects)
         self.play(FadeOutAndShiftDown(all_contents))
+
+
+    def alkene_examples(self):
+        title = TextMobject("Some basic alkenes")
+
+        ethene = (
+            TextMobject("1. Ethene"),
+            TextMobject("2"),
+            TexMobject("C_2H_4"),
+            TexMobject("CH_2=CH_2"),
+            #ChemObject("C(-[2]H)(-[4]H)(-[6]H)-C(-[2]H)(-[6]H)(-[8]H)"),
+            TexMobject("\chemfig{C(-[2]H)(-[6]H)=C(-[2]H)(-[6]H)}"),
+        )
+        propene = (
+            TextMobject("2. Propene"),
+            TextMobject("3"),
+            TexMobject("C_3H_6"),
+            TexMobject("CH_2=CH_2-CH_3"),
+            #ChemObject("C(-[2]H)(-[4]H)(-[6]H)-C(-[2]H)(-[6]H)-C(-[2]H)(-[6]H)(-[8]H)"),
+            TexMobject("\chemfig{C(-[2]H)(-[6]H)=C(-[2]H)-C(-[2]H)(-[6]H)(-[8]H)}"),
+        )
+        butene = (
+            TextMobject("3. Butene"),
+            TextMobject("4"),
+            TexMobject("C_4H_8"),
+            TexMobject("CH_3-CH=CH-CH_3"),
+            TexMobject(
+                "\chemfig{C(-[2]H)(-[4]H)(-[6]H)-C(-[2]H)=C(-[2]H)-C(-[2]H)(-[6]H)(-[8]H)}"
+            ),
+        )
+        title.set_color(BLUE).scale(1.5)
+        title.to_edge(UP)
+        self.play(Write(title))
+        [
+            self.display_properties(i, relative_to=title)
+            for i in (ethene, propene, butene)
+        ]
+        self.play(FadeOut(title))
+
+
+    def alkyne_intro(self):
+        intro = (
+            "Alkynes" 
+            "Those hydrocarbons in which at least one carbon to carbon",
+            "bond is triple covalent bond while the rest maybe triple,",
+            "double or single covalent bonds are called alkynes.",
+            "Its general formula is $C_{n}H_{2n-2}$ where n>1",
+        )
+
+        color_map = {
+            1: {"at": ORANGE, "least": ORANGE, "one": ORANGE},
+            2: {"triple": ORANGE, "covalent": ORANGE},
+            4: {"C": BLUE, "n>1": ORANGE},
+        }
+
+        self.general_formula = self.display_simple_info(
+            *intro,
+            color_map=color_map,
+            center=True,
+            skip_exit_anim_and_return=[i for i in range(len(intro))],
+        )[-1]
+
+    def alkyne_mol_formulas(self):
+        self.molecular_formula = TextMobject("Molecular formulas;")
+        self.molecular_formula.next_to(self.general_formula, DOWN, buff=0.6).to_edge(
+            LEFT
+        )
+        self.play(Write(self.molecular_formula))
+        gen_formula = (
+            "C_n",
+            "H_{2n-2}",
+        )
+        ethyne = [
+            "ethyne",
+            "take n = 2",
+            gen_formula,
+            ("C_2", "H_{2 \\times 2 - 2}"),
+            ("C_2", "H_{2},"),
+        ]
+        propyne = [
+            "propyne",
+            "take n = 3",
+            gen_formula,
+            ("C_3", "H_{2 \\times 3 - 2}"),
+            ("C_3", "H_{4},"),
+        ]
+        butyne = [
+            "butyne",
+            "take n = 4",
+            gen_formula,
+            ("C_4", "H_{2 \\times 4 - 2}"),
+            ("C_4", "H_{6},"),
+        ]
+
+        processed = []
+        for molecule in (ethyne, propyne, butyne):
+            last_molecule = processed[-1] if processed else None
+            formula = self.show_molecular_animations(molecule, last_molecule)
+            processed.append(formula)
+
+        self.wait()
+        all_contents = Group(*self.mobjects)
+        self.play(FadeOutAndShiftDown(all_contents))
+
+
+    def alkyne_examples(self):
+        title = TextMobject("Some basic alkynes")
+
+        ethyne = (
+            TextMobject("1. Ethyne"),
+            TextMobject("2"),
+            TexMobject("C_2H_2"),
+            #ChemObject("CH~CH"),
+            TexMobject("CH~CH"),
+            #ChemObject("C(-[2]H)(-[4]H)(-[6]H)-C(-[2]H)(-[6]H)(-[8]H)"),
+            TexMobject("\chemfig{C(-[2]H)~C(-[2]H)}"),
+        )
+        propyne = (
+            TextMobject("2. Propyne"),
+            TextMobject("3"),
+            TexMobject("C_3H_4"),
+            TexMobject("CH_2=CH_2-CH_3"),
+            #ChemObject("C(-[2]H)(-[4]H)(-[6]H)-C(-[2]H)(-[6]H)-C(-[2]H)(-[6]H)(-[8]H)"),
+            TexMobject("\chemfig{C(-[2]H)~C-C(-[2]H)(-[6]H)(-[8]H)}"),
+        )
+        butene = (
+            TextMobject("3. Butene"),
+            TextMobject("4"),
+            TexMobject("C_4H_8"),
+            TexMobject("CH_3-CH=CH-CH_3"),
+            TexMobject(
+                "\chemfig{C(-[2]H)(-[4]H)(-[6]H)-C(-[2]H)=C(-[2]H)-C(-[2]H)(-[6]H)(-[8]H)}"
+            ),
+        )
+        title.set_color(BLUE).scale(1.5)
+        title.to_edge(UP)
+        self.play(Write(title))
+        [
+            self.display_properties(i, relative_to=title)
+            for i in (ethene, propene, butene)
+        ]
+        self.play(FadeOut(title))
+
+
 
     def show_molecular_animations(self, molecule, last_molecule):
         processed = []
@@ -138,7 +286,7 @@ class SaturatedHydrocarbon(Scene):
             self.wait()
 
         target = formula.generate_target().next_to(
-            self.molecular_formula, DOWN, buff=1.0
+            self.molecular_formula, DOWN, buff=0.6
         )
         target = (
             target.next_to(last_molecule, buff=1.5)
@@ -146,123 +294,9 @@ class SaturatedHydrocarbon(Scene):
             else target.to_edge(LEFT)
         )
         self.play(MoveToTarget(formula), FadeOut(let_part))
-        formula_label.next_to(formula, DOWN, buff=0.5)
+        formula_label.next_to(formula, DOWN, buff=0.3)
         self.play(Write(formula_label))
         return formula
-
-    def word_roots_table(self):
-        title = TextMobject("Word roots table for saturated hydrocarbons")
-        carbon_atoms_col = [
-            "No. of C atoms",
-            "1",
-            "2",
-            "3",
-            "4",
-            "5",
-            "6",
-        ]
-        symbol_col = [
-            "Symbol",
-            "$C_1$",
-            "$C_2$",
-            "$C_3$",
-            "$C_4$",
-            "$C_5$",
-            "$C_6$",
-        ]
-        word_root_col = [
-            "Word root",
-            "Meth",
-            "Eth",
-            "Prop",
-            "But",
-            "Pent",
-            "Hex",
-        ]
-
-        title.set_color(BLUE)
-        title.to_edge(UP)
-        carbon_atoms = self.makeColGroup(carbon_atoms_col)
-        carbon_atoms.next_to(title, DOWN, buff=1.0).to_edge(LEFT).shift(RIGHT)
-        symbols = self.makeColGroup(symbol_col)
-        symbols.next_to(carbon_atoms, buff=1.0)
-        word_roots = self.makeColGroup(word_root_col)
-        word_roots.next_to(symbols, buff=1.0)
-
-        contents = [title, carbon_atoms, symbols, word_roots]
-        [self.play(Write(i)) for i in contents]
-        self.wait(4)
-        self.play(FadeOutAndShiftDown(VGroup(*contents)))
-
-    def makeColGroup(self, texts, tex_class=TextMobject, spacing=0.5, text_scale=1.0):
-        text_objects = []
-        for text in texts:
-            obj = None
-            if isinstance(text, str):
-                singlify = lambda x: [f"{i} " for i in x.split(" ")]
-                obj = TextMobject(*singlify(text))
-            elif isinstance(text, TexMobject):
-                obj = text
-            elif isinstance(text, tuple):
-                objects = [tex_class(i) for i in text]
-                processed = [objects[0]]
-                for i in objects[1:]:
-                    i.next_to(processed[-1], DOWN, buff=0.2)
-                    processed.append(i)
-                obj = VGroup(*processed)
-            else:
-                AssertionError(f"Unsupported type of {type(text)}: {text}")
-
-            obj.scale(text_scale)
-            text_objects.append(obj)
-
-        first = text_objects[0]
-        processed = [first]
-        for obj in text_objects[1:]:
-            obj.next_to(processed[-1], DOWN, buff=spacing)
-            processed.append(obj)
-
-        return VGroup(*processed)
-
-    def hydrocarbons_properties(self):
-        title = TextMobject("Some saturated hydrocarbons")
-        methane = (
-            TextMobject("1. Methane"),
-            TextMobject("1"),
-            TexMobject("CH_4"),
-            TexMobject("CH_4"),
-            ChemObject("C(-[0]H)(-[2]H)(-[4]H)(-[6]H)"),
-        )
-        ethane = (
-            TextMobject("2. Ethane"),
-            TextMobject("2"),
-            TexMobject("C_2H_6"),
-            TexMobject("CH_3-CH_3"),
-            ChemObject("C(-[2]H)(-[4]H)(-[6]H)-C(-[2]H)(-[6]H)(-[8]H)"),
-        )
-        propane = (
-            TextMobject("3. Propane"),
-            TextMobject("3"),
-            TexMobject("C_3H_8"),
-            TexMobject("CH_3-CH_2-CH_3"),
-            ChemObject("C(-[2]H)(-[4]H)(-[6]H)-C(-[2]H)(-[6]H)-C(-[2]H)(-[6]H)(-[8]H)"),
-        )
-        butane = (
-            TextMobject("4. Butane"),
-            TextMobject("4"),
-            TexMobject("C_4H_{10}"),
-            TexMobject("CH_3-CH_2-CH_2-CH_3"),
-            ChemObject(
-                "C(-[2]H)(-[4]H)(-[6]H)-C(-[2]H)(-[6]H)-C(-[2]H)(-[6]H)-C(-[2]H)(-[6]H)(-[8]H)"
-            ),
-        )
-        title.set_color(BLUE).scale(1.5)
-        title.to_edge(UP)
-        self.play(Write(title))
-        [
-            self.display_properties(i, relative_to=title)
-            for i in (methane, ethane, propane, butane)
-        ]
 
     def display_properties(self, properties, relative_to=None):
         name, c_atoms, mol_formula, cond_formula, structure = properties
