b8c5f1ee3a8fb5f9dc7e2cef5264e22a780a013f
hemanta212 <sharmahemanta.212@gmail.com>
2021-04-11 14:17:47 +0000

    Finish geometric angle between vectors calculation

--- temp.py
+++ temp.py
@@ -5,14 +5,14 @@ class Test(Scene):
     scalar_topic = None
 
     def construct(self):
-        self.topic()
-        self.intro()
-        self.vector_intro_graph()
-        self.algebric_calc()
-        self.geometric_interpretation()
-        self.geometric_calc()
-        self.case_of_perpendicularity()
-        # self.angle_of_vectors()
+        # self.topic()
+        # self.intro()
+        # self.vector_intro_graph()
+        # self.algebric_calc()
+        # self.geometric_interpretation()
+        # self.geometric_calc()
+        # self.case_of_perpendicularity()
+        self.angle_of_vectors()
         self.wait(3)
 
     def topic(self):
@@ -54,7 +54,6 @@ class Test(Scene):
         intro_text = VGroup(*[TextMobject(i) for i in intro])
         intro_text.arrange(DOWN, center=False).shift(UP)
         self.play(Write(intro_text), run_time=8)
-
         for_eg_text = TextMobject(
             r"Let us take two vectors $\vec{A}$ = 2i + 2j and $\vec{B}$ = 2i, ",
         )
@@ -213,8 +212,7 @@ class Test(Scene):
         vb_brace_tex.generate_target().next_to(va_tar)
         va_dot_vb_g = VGroup(va_proj_tex, vb_brace_tex)
         va_dot_vb_exp = TextMobject(
-            "(Length of proj. of $\\vec{A}$) $\\cdot$ "
-            "(Length of $\\vec{B}$)"
+            "(Length of proj. of $\\vec{A}$) $\\cdot$ " "(Length of $\\vec{B}$)"
         ).next_to(a_b)
 
         alternatively = TextMobject("Alternatively,", color=BLUE).scale(1.5).to_edge(UL)
@@ -295,11 +293,11 @@ class Test(Scene):
 
         va_tex = TexMobject("\\vec{A}")
         vb_tex = TexMobject("\\vec{B}")
-        va_tex.next_to(va.get_end(), UL)
-        vb_tex.next_to(vb.get_end(), UR)
+        va_tex.add_updater(lambda x: x.next_to(va.get_end(), UL))
+        vb_tex.add_updater(lambda x: x.next_to(vb.get_end(), UR))
 
         va_extension = Line(va.get_start(), va.get_end()).scale(5)
-        vb_obtuse = vb.generate_target().rotate(-PI/2, about_edge=LEFT)
+        vb_obtuse = vb.generate_target().rotate(-PI / 2, about_edge=LEFT)
 
         vb_proj = self.get_vect_mob_projection(vb_obtuse, va)
         vb_perp_drop = Line(vb_obtuse.get_end(), vb_proj.get_end(), color=BLUE)
@@ -307,29 +305,24 @@ class Test(Scene):
         a_b = TexMobject(r"\vec{A} \cdot \vec{B} =").to_edge(UL)
 
         vb_dot_va_exp = TextMobject(
-            "- ", " (Length of proj. of $\\vec{B}$) $\\cdot$ "
-            "(Length of $\\vec{A}$)"
+            "- ", " (Length of proj. of $\\vec{B}$) $\\cdot$ " "(Length of $\\vec{A}$)"
         ).next_to(a_b)
 
         vb_dot_va_exp[0].set_color(YELLOW).scale(2)
 
         case_of_perp = TextMobject("Case of perpendicularity", color=BLUE).scale(1.5)
         case_of_perp.to_edge(UL)
-        vb_right = vb.copy().rotate(-PI/4, about_edge=LEFT)
-
+        vb_right = vb.copy().rotate(-PI / 4, about_edge=LEFT)
+        vb_right_angle_indicator = Arc().set_points_as_corners(
+            [UR / 3, RIGHT / 1.5, DR / 3]
+        )
         vb_dot_va_exp_right = TextMobject(
-            "0 $\\cdot$ "
-            "(Length of $\\vec{A}$)"
+            "0 $\\cdot$ " "(Length of $\\vec{A}$)"
         ).next_to(a_b)
 
-        vb_dot_va_exp_right2 = TextMobject(
-            "0"
-        ).next_to(a_b)
-
-        vb_tex.add_updater(lambda x: x.next_to(vb.get_end(), UR))
+        vb_dot_va_exp_right2 = TextMobject("0").next_to(a_b)
 
         self.add(grid, va, va_tex, vb, vb_tex, a_b)
-
         self.play(ShowCreation(va_extension))
         self.play(MoveToTarget(vb))
         self.play(ShowCreation(vb_perp_drop))
@@ -343,8 +336,9 @@ class Test(Scene):
 
         self.play(Write(case_of_perp))
         self.wait()
-        
+
         self.play(Transform(vb, vb_right))
+        self.play(ShowCreation(vb_right_angle_indicator))
         self.wait()
 
         self.play(ReplacementTransform(case_of_perp, a_b))
@@ -352,3 +346,148 @@ class Test(Scene):
         self.wait()
         self.play(ReplacementTransform(vb_dot_va_exp_right, vb_dot_va_exp_right2))
         self.wait()
+
+    def angle_of_vectors(self):
+        axes = Axes(x_min=-1, y_min=0)
+        va = (
+            Vector(direction=UP + RIGHT / 2, color=GREEN)
+            .scale(3)
+            .shift((UP + RIGHT / 2))
+        )
+        vb = Vector(direction=UR, color=BLUE).scale(2).shift(UR / 2)
+
+        x_tex = axes.get_x_axis_label("X")
+        o_tex = TexMobject("O").shift(DOWN / 2)
+        va_tex = TexMobject("\\vec{A}")
+        vb_tex = TexMobject("\\vec{B}")
+        va_tex.add_updater(lambda x: x.next_to(va.get_end(), UL))
+        vb_tex.add_updater(lambda x: x.next_to(vb.get_end(), UR))
+
+        given = (
+            "Here, angle between $\\vec{A}$ and $\\vec{B}$ = $\\angle{AOB}$",
+            "Angle made with X-axis;",
+            "by $\\vec{A}$ = $\\angle{XOA}$",
+            "by $\\vec{B}$ = $\\angle{XOB}$",
+            "From figure, we can say, $\\angle{AOB}$ = $\\angle{XOA} - \\angle{XOB}$",
+        )
+        given_g = VGroup(*[TextMobject(i) for i in given]).arrange(DOWN, center=False)
+        given_g.scale(0.8).to_edge(DOWN, buff=0.1)
+
+        tri_xoa = (
+            "Let us now look at $\\triangle{AOD}$ to find $\\angle{XOA}$",
+            "From distance formula, OA, OD can be obtained.",
+            "Now, $cos \\theta = \\frac{b}{h} = \\frac{OD}{OA}$"
+            " or, cos$\\theta = \\frac{ \\sqrt{a_1^2} }{ \\sqrt{a_1^2 + b_1^2} } = \\frac{a_1}{| \\vec{A} |}$",
+            "$\\therefore \\theta = cos^{-1}(\\frac{a_1}{| \\vec{A} |})$",
+        )
+        tri_xoa_g = VGroup(*[TextMobject(i) for i in tri_xoa])
+        tri_xoa_g.arrange(DOWN, center=False).scale(0.9).to_edge(DOWN, buff=0.1)
+
+        side_AD = Line(va.get_end(), va.get_end() + 3 * DOWN, color=GREEN)
+        label_D = TextMobject("D").next_to(side_AD.get_end(), DOWN)
+        coor_tex_OA = TexMobject(r"(a_1,b_1)").next_to(va_tex)
+        coor_tex_OD = TexMobject("(a_1,0)").next_to(label_D)
+        angle_OA_point = va.get_start() + (UP + RIGHT / 2) / 3
+        angle_indicator = ArcBetweenPoints(
+            start=angle_OA_point, end=RIGHT / 3, angle=-TAU / 4, color=YELLOW
+        )
+        theta_tex = TexMobject("\\theta").scale(0.8)
+        theta_tex.next_to(va.get_start(), buff=0.5).shift(UP / 4)
+        tri_xoa_animations = [
+            (
+                ShowCreation(side_AD),
+                ShowCreation(label_D),
+                FadeOut(vb),
+                ShowCreation(coor_tex_OA),
+                ShowCreation(coor_tex_OD),
+            ),
+            (
+                ShowCreation(angle_indicator),
+                FadeIn(theta_tex),
+            ),
+            (Animation(va),),
+            (Animation(va),),
+        ]
+
+        tri_xob = (
+            "Similarly, for $\\triangle{BOE}$, we can find $\\angle{XOB}$.",
+            "Here, $cos \\alpha = \\frac{b}{h} = \\frac{OE}{OB}$"
+            " or, cos$\\alpha = \\frac{ \\sqrt{a_2^2} }{ \\sqrt{a_2^2 + b_2^2} } = \\frac{a_2}{| \\vec{B} |}$",
+            "$\\therefore \\alpha = cos^{-1}(\\frac{a_2}{| \\vec{B} |})$",
+        )
+        tri_xob_g = VGroup(*[TextMobject(i) for i in tri_xob])
+        tri_xob_g.arrange(DOWN, center=False).scale(0.9).to_edge(DOWN, buff=0.1)
+        side_BE = Line(vb.get_end(), vb.get_end() + 2 * DOWN, color=BLUE)
+        label_E = TextMobject("E").next_to(side_BE.get_end(), DOWN)
+        coor_tex_OB = TexMobject(r"(a_2,b_2)").next_to(vb_tex)
+        coor_tex_OE = TexMobject("(a_2,0)").next_to(label_E)
+        angle_OB_point = vb.get_start() + UR / 3
+        angle_XOB_indicator = ArcBetweenPoints(
+            start=angle_OB_point, end=RIGHT / 3, angle=-TAU / 4, color=YELLOW
+        )
+        alpha_tex = TexMobject("\\alpha").scale(0.8)
+        alpha_tex.next_to(va.get_start(), buff=0.5).shift(UP / 4)
+        tri_xob_animations = [
+            (
+                FadeOut(VGroup(side_AD, label_D, angle_indicator, theta_tex)),
+                FadeOut(VGroup(va, coor_tex_OA, coor_tex_OD, va_tex)),
+                FadeIn(vb),
+                ShowCreation(side_BE),
+                ShowCreation(label_E),
+                ShowCreation(coor_tex_OB),
+                ShowCreation(coor_tex_OE),
+            ),
+            (
+                ShowCreation(angle_XOB_indicator),
+                FadeIn(alpha_tex),
+            ),
+            (Animation(vb),),
+            (Animation(vb),),
+        ]
+        conclusion = (
+            "Let's now look at the initial figure,",
+            "As discussed earlier, $\\angle{AOB}$ = $\\angle{XOA}(\\theta) - \\angle{XOB}(\\alpha)$",
+            "or, $\\angle{AOB} = cos^{-1}(\\frac{a_1}{| \\vec{A} |}) - cos^{-1}(\\frac{a_2}{| \\vec{B} |})$",
+            "$\\therefore$ The angle between vectors is obtained.",
+        )
+        conclusion_g = VGroup(*[TextMobject(i) for i in conclusion])
+        conclusion_g.arrange(DOWN, center=False).scale(0.9).to_edge(DOWN, buff=0.1)
+
+        angle_BOA_indicator = ArcBetweenPoints(
+            start=angle_OA_point, end=angle_OB_point, angle=-TAU / 4, color=YELLOW
+        )
+        conclusion_animations = [
+            (
+                FadeOut(VGroup(side_BE, label_E, angle_XOB_indicator, alpha_tex)),
+                FadeOut(VGroup(coor_tex_OB, coor_tex_OE)),
+                FadeIn(VGroup(va, va_tex)),
+            ),
+            (ShowCreation(angle_BOA_indicator),),
+            (Animation(vb),),
+            (Animation(vb),),
+        ]
+
+        self.play(ShowCreation(axes), FadeIn(x_tex), FadeIn(o_tex))
+        self.play(ShowCreation(VGroup(va, va_tex)))
+        self.play(ShowCreation(VGroup(vb, vb_tex)))
+        # for line in given_g:
+        #     self.play(Write(line), run_time=3)
+        #     self.wait()
+        # self.wait(3)
+        # self.play(FadeOut(given_g))
+        for text, anim in zip(tri_xoa_g, tri_xoa_animations):
+            self.play(Write(text), *anim, run_time=5)
+            self.wait()
+        self.wait(3)
+        self.play(FadeOut(tri_xoa_g))
+
+        for text, anim in zip(tri_xob_g, tri_xob_animations):
+            self.play(Write(text), *anim, run_time=5)
+            self.wait()
+        self.wait(3)
+        self.play(FadeOut(tri_xob_g))
+
+        for text, anim in zip(conclusion_g, conclusion_animations):
+            self.play(Write(text), *anim, run_time=5)
+            self.wait()
+        self.wait(3)
